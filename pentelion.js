8baaf1d4d0a404cc52b9ae959d19d020121480af
FWeichenmeier <florian.weichenmeier@gmx.de>
2015-04-06 17:25:09 +0200

    first version
    first version

---
+++ pentelion.js
@@ -0,0 +1,49 @@
+function calcValues(sg, vg, sk) {
+    "use strict";
+    var res = [],
+        i,
+        s = sg - vg;
+    for (i = -2; i < 3; i = i + 1) {
+        s = sg - vg + i*10;
+        res.push([ s, 50-s, 75-s, 90-s, 100-s]);
+    }
+    return res;
+}
+
+function calcTable(sg, vg, sk) {
+    "use strict";
+    var table = document.getElementById("table"),
+        res = calcValues(sg, vg, sk),
+        s = "<table><tr><td>Diff</td><td>+</td><td>+E</td><td>+G</td><td>+K</td></tr>",
+        i,
+        j;
+    for (i = 0; i < res.length; ++i) {
+        s += "<tr>";
+        for (j = 0; j < res[i].length; ++j) {
+            s += "<td>" + res[i][j] + "</td>";
+        }
+        s += "</tr>";        
+    }
+    s += "</table>";
+    table.innerHTML = s;
+}
+
+function calculate() {
+    "use strict";
+    calcTable(document.getElementById("KG").value, document.getElementById("VG").value, document.getElementById("SK").value)
+}
+
+function processValues(el1, el2) {
+    "use strict";
+    document.getElementById(el1).value = document.getElementById(el2).value;
+    calculate();
+}
+
+function onStart() {
+    "use strict";
+    document.getElementById("KGtext").value = document.getElementById("KG").value;
+    document.getElementById("VGtext").value = document.getElementById("VG").value;
+    document.getElementById("SKtext").value = document.getElementById("SK").value;
+    calculate();
+}
+
